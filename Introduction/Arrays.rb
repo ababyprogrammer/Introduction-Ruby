5a6008321bd345d1e8cc1b70cd2b99320cb991c3
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2024-03-21 11:08:41 +0330

    Add files via upload

---
+++ Introduction/Arrays.rb
@@ -0,0 +1,6 @@
+name = ["May", "A baby programmer", "David"]
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[2]
+puts fruits[3]
---
+++ Introduction/Basic-arithmetic-operators.rb
@@ -0,0 +1,9 @@
+a = 5
+b = 2
+
+puts a + b
+puts a * b
+puts a - b
+puts a / b
+puts a ** b
+puts a % b
